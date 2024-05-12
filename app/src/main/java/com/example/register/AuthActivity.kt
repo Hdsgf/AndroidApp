30b621d33d65dbc2f937440d23b1e99f7cc1de1e
Hdsgf <zubelevichivan@gmail.com>
2024-05-13 00:10:59 +0300

    Login&Register

--- app/src/main/java/com/example/register/AuthActivity.kt
+++ app/src/main/java/com/example/register/AuthActivity.kt
@@ -2,7 +2,10 @@ package com.example.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,11 +22,35 @@ class AuthActivity : AppCompatActivity() {
             insets
         }
 
+        val userLogin: EditText = findViewById(R.id.user_login_auth)
+        val userPass: EditText = findViewById(R.id.user_password_auth)
+        val button: Button = findViewById(R.id.button_auth)
         val linkToReg: TextView = findViewById(R.id.link_to_reg)
 
         linkToReg.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        button.setOnClickListener {
+            val login = userLogin.text.toString().trim()
+            val pass = userPass.text.toString().trim()
+
+            if(login.isEmpty() || pass.isEmpty() ){
+                Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
+            }else {
+                val db = DbHelper(this, null)
+
+                val isAuth = db.getUser(login, pass)
+
+                if(isAuth) {
+                    Toast.makeText(this, "Пользователь $login авторизован", Toast.LENGTH_LONG).show()
+                    userLogin.text.clear()
+                    userPass.text.clear()
+                }else{
+                    Toast.makeText(this, "Пользователь $login Не найден", Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
